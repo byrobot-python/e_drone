8978358f1265d74ad8926d398abe7acc3fb3dda2
Sanghyo Lee <realwolf@naver.com>
2018-12-05 10:23:41 +0900

    "0.1.13" 릴리즈 / 파이썬 3.6 -> 3.7로 변경

--- build.bat
+++ build.bat
@@ -4,19 +4,19 @@ rem 이전 배포판 삭제
 del /f /q .\dist\*.*
 
 rem 삭제
-"C:\Users\Sanghyo Lee\Anaconda3\Scripts\pip.exe" uninstall e_drone -q
+"C:\ProgramData\Anaconda3\Scripts\pip.exe" uninstall e_drone -q
 
 rem 빌드
-"C:\Users\Sanghyo Lee\Anaconda3\python.exe" setup.py sdist
+"C:\ProgramData\Anaconda3\python.exe" setup.py sdist
 
 rem wheel 빌드
-"C:\Users\Sanghyo Lee\Anaconda3\python.exe" setup.py bdist_wheel
+"C:\ProgramData\Anaconda3\python.exe" setup.py bdist_wheel
 
 rem PYPI 등록
-"C:\Users\Sanghyo Lee\Anaconda3\Scripts\twine.exe" upload dist/*
-rem "C:\Users\Sanghyo Lee\Anaconda3\Scripts\twine.exe" upload --repository-url https://test.pypi.org/legacy/ dist/*
+"C:\ProgramData\Anaconda3\Scripts\twine.exe" upload dist/*
+rem "C:\ProgramData\Anaconda3\Scripts\twine.exe" upload --repository-url https://test.pypi.org/legacy/ dist/*
 
 rem 설치
-"C:\Users\Sanghyo Lee\Anaconda3\Scripts\pip.exe" --no-cache-dir install e_drone
+"C:\ProgramData\Anaconda3\Scripts\pip.exe" --no-cache-dir install e_drone
 
 pause
